285625695144b4e2592312a2743ae20e7bd6229d
M A Razvi <abuzarflw@gmail.com>
2022-06-04 12:08:07 -0700

    Create logic for skip card

--- src/components/Game.js
+++ src/components/Game.js
@@ -9,8 +9,8 @@ const Game = () => {
   const [playedCard, setPlayedCard] = useState("");
   const [p1Cards, setP1Cards] = useState([]);
   const [p2Cards, setP2Cards] = useState([]);
-  const [p1remainingTurns, setP1RemainingTurns] = useState(1);
-  const [p2remainingTurns, setP2RemainingTurns] = useState(0);
+  const [p1RemainingTurns, setP1RemainingTurns] = useState(1);
+  const [p2RemainingTurns, setP2RemainingTurns] = useState(0);
   const [activePlayer, setActivePlayer] = useState("P1");
 
   // BS = blasting shark
@@ -55,7 +55,29 @@ const Game = () => {
 
   }, []);
 
-  
+  function cardPlayedHandler(cardPlayed) {
+    const cardPlayedBy = activePlayer;
+    // let playerRemainingTurns;
+    // cardPlayedBy === "P1" ? playerRemainingTurns = p1RemainingTurns : playerRemainingTurns = p2RemainingTurns;
+
+    switch(playedCard) {
+     
+      //Logic for shuffle card
+      case "SH" : {
+        const drawDeck = [...drawCardsPile];
+        drawDeck = shuffler(drawDeck);
+        setDrawCardsPile([...drawDeck]);
+        break;
+      }
+
+      case "SK" : {
+
+      }
+
+      default : {console.log("Error");};
+    }
+
+  }
   
 return (
     <div>
