2ab462a4c4b116d41f6a4c05eda165f610095cef
mateonav98 <mateonav98@gmail.com>
2022-06-05 13:52:46 -0700

    add login with token

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -20,6 +20,15 @@ class AuthService {
           return false;
         }
       }
+      getToken() {
+        return localStorage.getItem('id_token');
+      }
+      login(idToken) {
+        // Saves user token to localStorage
+        localStorage.setItem('id_token', idToken);
+        window.location.assign('/');
+      }
+   
 }
 
 export default new AuthService();
