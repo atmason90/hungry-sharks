f66e8eaf6d64e7c1d88075a1dbb4da7cd98ccb3d
mateonav98 <mateonav98@gmail.com>
2022-06-05 10:39:35 -0700

    check if token is expired

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -10,6 +10,16 @@ class AuthService {
       const token = this.getToken();
       return !!token && !this.isTokenExpired(token);
     }
+    isTokenExpired(token) {
+        try {
+          const decoded = decode(token);
+          if (decoded.exp < Date.now() / 1000) {
+            return true;
+          } else return false;
+        } catch (err) {
+          return false;
+        }
+      }
 }
 
 export default new AuthService();
