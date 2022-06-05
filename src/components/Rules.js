0756cb9255dcc12464ba56cd251f119b225ab970
atmason90 <atmason90@gmail.com>
2022-06-05 14:25:19 -0700

    begin adding rules to rules component

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -4,7 +4,14 @@ const Rules = () => {
   return (
     <div>
       <h1>Rules of the Game</h1>
-      
+      <div>
+      <h2>The Basics</h2>
+        <ul>
+            <li>In the deck of cards are some Hungry Sharks. You play the game by putting the deck face down and taking turns drawing cards until someone draws a Hungry Shark</li>
+            <li>When that happens, that person is eaten by the Hungry Shark, and the other player wins the game.</li>
+            <li></li>
+        </ul>
+      </div>
     </div>
   )
 }
