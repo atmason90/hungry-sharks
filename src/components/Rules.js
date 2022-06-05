fbbdbec24aa5ea9fa5ea5976605791415505cf4d
atmason90 <atmason90@gmail.com>
2022-06-05 14:31:51 -0700

    import assets to rules component

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import CR from '../assets/CR.jpeg';
+import DR from '../assets/DR.jpeg';
+import DT from '../assets/DT.jpeg';
+import HS from '../assets/HS.jpeg';
+import SG from '../assets/SG.jpeg';
+import SH from '../assets/SH.jpeg';
+import SN from '../assets/SN.jpeg';
+import WC from '../assets/WC/jpeg';
 
 const Rules = () => {
   return (
@@ -9,8 +17,21 @@ const Rules = () => {
         <ul>
             <li>In the deck of cards are some Hungry Sharks. You play the game by putting the deck face down and taking turns drawing cards until someone draws a Hungry Shark</li>
             <li>When that happens, that person is eaten by the Hungry Shark, and the other player wins the game.</li>
-            <li></li>
         </ul>
+      </div>
+      <div>
+      <h2>The Cards</h2>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>    
+        </ul>    
       </div>
     </div>
   )
