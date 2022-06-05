0e0a60e6a7c756ea7608038f977ebd4cda13f3ff
atmason90 <atmason90@gmail.com>
2022-06-04 19:47:55 -0700

    create basic login form component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const validateForm = () => {
+      return username.length > 0 && password.length > 0;
+  }
+
+  const handleFormSubmit = (event) => {
+      event.preventDefault();
+  }
+    
   return (
     <div>
-      
+      <Form>
+          <Form.Group>
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                autoFocus
+                type='String'
+                value='username'
+                onChange={(e) => setUsername(e.target.value)}
+              />  
+          </Form.Group>
+          <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                value='password'
+                onChange={(e) => setPassword(e.target.value)}
+              />
+          </Form.Group>
+          <Button type='submit'>Login</Button>
+      </Form>
     </div>
   )
 }
