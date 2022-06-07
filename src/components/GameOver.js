056c2a6884dec8ede67a6dbab0a4a56075888c69
atmason90 <atmason90@gmail.com>
2022-06-07 11:21:15 -0700

    add buttons to gameover component

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Link from 'react-router-dom';
 
-const GameOver = () => {
+
+
+const GameOver = ({ winner }) => {
   return (
     <div>
       <h1>Game Over!</h1>
       {/* line for which player won */}
-
+      <h2>{winner} wins!</h2>
       {/* button asking if want to rematch */}
-      
+      <Link to="/game">
+        <button>Rematch?</button>
+      </Link>
       {/* button to return to home page */}
+      <Link to='/home'>
+        <button>Home</button>
+      </Link>
+      {/* button to view highscores page */}
+      <Link to='/highscores'>
+        <button>View Stats</button>
+      </Link>
     </div>
   )
 }
