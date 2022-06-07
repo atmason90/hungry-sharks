3380d483ea2587fb25758a937308824680f41d4e
atmason90 <atmason90@gmail.com>
2022-06-07 11:14:10 -0700

    add buttons to gameover component

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -4,9 +4,11 @@ const GameOver = () => {
   return (
     <div>
       <h1>Game Over!</h1>
-      {/* add line for which player won */}
-      {/* add button asking if want to rematch */}
-      {/* add button to return to home page */}
+      {/* line for which player won */}
+
+      {/* button asking if want to rematch */}
+      
+      {/* button to return to home page */}
     </div>
   )
 }
