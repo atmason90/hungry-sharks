7ea8c82acfb8bcee5a4c052d186387680793642d
atmason90 <atmason90@gmail.com>
2022-06-07 22:00:20 -0700

    attempt highscores component

--- client/src/components/HighScores.js
+++ client/src/components/HighScores.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Auth from '../utils/auth';
+import { getHighscores, getSingleHighscore } from '../utils/API';
+import userController from '../../../server/controllers/user-controller';
 
-const HighScores = () => {
+const Highscores = () => {
+  // const [getHighscores, setHighscores] = useState([]);
+
+  const [getSingleHighscore, setSingleHighscores] = useState([]);
+
+  // useEffect(() => {
+  //   return () => 
+  // })
+
+  const userData = items.map((user) => ({
+    games: user.games,
+    wins: user.wins,
+    losses: user.losses
+  }));
+
+  setSingleHighscores(userData);
+
   return (
     <div>
       {/* figure out how to display data from mongodb */}
+      <h2>
+        Games Played: {user.games}
+      </h2>
       
     </div>
   )
