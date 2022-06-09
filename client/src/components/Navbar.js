e8a266905de615daa86609d36e1f27e720afedd3
mateonav98 <mateonav98@gmail.com>
2022-06-08 18:00:36 -0700

    centering nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,19 +12,19 @@ const AppNavbar = () => {
 
   return (
     <>
-      <Navbar className="flex-row items-center justify-between px-6" bg='black' variant='dark' expand='lg'>
-        <Container className="flex flex-row items-center " fluid>
+      <Navbar className="flex flex-row items-center justify-center px-6" bg='black' variant='dark' expand='lg'>
+        <Container className="flex flex-row items-center justify-center" fluid>
           <Navbar.Brand 
-          className="flex-row items-center bg-[#000000] hover:bg-orange-700 text-white font-bold py-2 px-4 border border-[#f06c00] rounded mr-10"
+          className="bg-[#000000] hover:bg-orange-700 text-white font-bold py-2 px-4 border border-[#f06c00] rounded mr-10"
           as={Link} to='/'>
             Home
           </Navbar.Brand>
           
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse  id='navbar'>
-            <Nav className='flex flex-row items-center  ml-auto'>
+            <Nav className='flex flex flex-row items-center  ml-auto'>
             <Nav.Link
-            className="flex-row items-center bg-[#000000] hover:bg-orange-700 text-white font-bold py-2 px-4 border border-[#f06c00] rounded mr-10"
+            className="bg-[#000000] hover:bg-orange-700 text-white font-bold py-2 px-4 border border-[#f06c00] rounded mr-10"
             as={Link} to='/rules'>
                 See Game Rules
               </Nav.Link>
@@ -33,21 +33,20 @@ const AppNavbar = () => {
               {Auth.loggedIn() ? (
                 <>
                   <Nav.Link 
-                  className="flex-row items-center bg-[#000000] hover:bg-orange-700 text-white font-bold py-2 px-4 border border-[#f06c00] rounded mr-10"
+                  className="bg-[#000000] hover:bg-orange-700 text-white font-bold py-2 px-4 border border-[#f06c00] rounded mr-10"
                   as={Link} to='/highscores'>
                     See HighScores
                   </Nav.Link>
                   <br></br>
                   <Nav.Link 
-                  className="flex-row items-center bg-[#000000] hover:bg-orange-700 text-white font-bold py-2 px-4 border border-[#f06c00] rounded mr-10"
+                  className="bg-[#000000] hover:bg-orange-700 text-white font-bold py-2 px-4 border border-[#f06c00] rounded mr-10"
                   onClick={Auth.logout}>Logout</Nav.Link>
                 </>
               ) : (
                 <Nav.Link 
-                className="flex-row items-center bg-[#000000] hover:bg-orange-700 text-white font-bold py-2 px-4 border border-[#f06c00] rounded mr-10"
+                className="flex flex-row items-center bg-[#000000] hover:bg-orange-700 text-white font-bold py-2 px-4 border border-[#f06c00] rounded mr-10"
                 as={Link} to='/login'>Login</Nav.Link>
               )
-            //   (<Nav.Link as={Link} to='/signup'>Sign Up</Nav.Link>)
               }
             </Nav>
           </Navbar.Collapse>
