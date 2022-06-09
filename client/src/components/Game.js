d4044eb620c877ffd542757a3f36188f1304cae2
M A Razvi <abuzarflw@gmail.com>
2022-06-09 14:33:41 -0700

    Render p1view and p2view components in Game.js

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -8,6 +8,7 @@ import ToggleP1 from "./ToggleP1";
 import ToggleP2 from "./ToggleP2";
 import ViewCardModal from "./ViewCardModal";
 import Player1View from "./Player1View";
+import Player2View from "./Player2View";
 
 let socket;
 const ENDPOINT = "http://localhost:3001";
@@ -755,88 +756,29 @@ const Game = () => {
               {/* P1 VIEW */}
               {currentUser === "Player 1" && (
                 <Player1View
-                cardPlayedHandler={cardPlayedHandler} p2Cards={p2Cards} activePlayer={activePlayer} p1Cards={p1Cards} fullname={fullname} drawCardHandler={drawCardHandler} info={info} playedCard={playedCard}
+                  cardPlayedHandler={cardPlayedHandler}
+                  p2Cards={p2Cards}
+                  activePlayer={activePlayer}
+                  p1Cards={p1Cards}
+                  fullname={fullname}
+                  drawCardHandler={drawCardHandler}
+                  info={info}
+                  playedCard={playedCard}
                 />
               )}
 
               {/* P2 VIEW */}
               {currentUser === "Player 2" && (
-                <>
-                  <div
-                    className="player1Deck"
-                    style={{ pointerEvents: "none" }}
-                  >
-                    <p className="playerDeckText text-orange-700">P1</p>
-                    {p1Cards.map((item, i) => (
-                      <div className="shadow-xl">
-                        <img
-                          key={i}
-                          className="Card"
-                          onClick={() => cardPlayedHandler(item)}
-                          src={require(`../assets/back.png`)}
-                          alt={`${fullname(item)}`}
-                        />
-                      </div>
-                    ))}
-                    {activePlayer === "P1"}
-                  </div>
-                  <br />
-                  <div
-                    className="middleInfo"
-                    style={
-                      activePlayer === "P1" ? { pointerEvents: "none" } : null
-                    }
-                  >
-                    <button
-                      className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg hover:bg-orange-700 border-orange-700 bg-red-700 bg-opacity-40"
-                      disabled={activePlayer !== "P2"}
-                      onClick={drawCardHandler}
-                    >
-                      DRAW CARD
-                    </button>
-                    <div className="card w-96 bg-[#051222] text-neutral-content shadow-2xl bg-opacity-40">
-                      <div className="card-body items-center text-center">
-                        <h2 className="card-title text-orange-700 text-2xl">
-                          Info
-                        </h2>
-                        <p className="text-gray-300 text-xl">{info}</p>
-                      </div>
-                    </div>
-                    {playedCard && (
-                      <div className="shadow-xl">
-                        <img
-                          className="Card"
-                          src={require(`../assets/${playedCard}.png`)}
-                          alt={`${fullname(playedCard)}`}
-                        />
-                        <h3 className="text-orange-700 mt-3">
-                          {fullname(playedCard)}
-                        </h3>
-                      </div>
-                    )}
-                  </div>
-                  <br />
-                  <div
-                    className="player2Deck"
-                    style={
-                      activePlayer === "P2" ? null : { pointerEvents: "none" }
-                    }
-                  >
-                    <p className="playerDeckText text-orange-600">P2</p>
-                    {p2Cards.map((item, i) => (
-                      <div className="player1DeckCards shadow-xl">
-                        <img
-                          key={i}
-                          className="Card"
-                          onClick={() => cardPlayedHandler(item)
-                          }
-                          src={require(`../assets/${item}.png`)}
-                          alt={`${fullname(item)}`}
-                        />
-                      </div>
-                    ))}
-                  </div>
-                </>
+                <Player2View
+                  cardPlayedHandler={cardPlayedHandler}
+                  p2Cards={p2Cards}
+                  activePlayer={activePlayer}
+                  p1Cards={p1Cards}
+                  fullname={fullname}
+                  drawCardHandler={drawCardHandler}
+                  info={info}
+                  playedCard={playedCard}
+                />
               )}
             </div>
           )}
