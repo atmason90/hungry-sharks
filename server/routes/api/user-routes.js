3fc778d912f1382cdf9cd7bf3bf996c8cee83fb6
M A Razvi <abuzarflw@gmail.com>
2022-06-10 16:57:35 -0700

    Remove auth middleware for new user

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -11,7 +11,7 @@ const {
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
 
-// put authMiddleware anywhere we need to send a token for verification of user
+// put authMiddleware anywhere we need to send a token for verification of user.
 router.route('/').post(createUser);
 
 router.route('/login').post(login);
