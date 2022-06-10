c98aeee3fefc571b088127b15548324fb11bc982
mateonav98 <mateonav98@gmail.com>
2022-06-09 20:19:02 -0700

    delete unused file

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
-
 import Auth from './utils/auth'
-
-
 
 
 function PrivateRoute({children}) {
@@ -14,12 +11,3 @@ function PrivateRoute({children}) {
 
 
 export default PrivateRoute
-
-
-// const PrivateRoute = ({children}) => {
-
-//   const [token_id, setJwt] = Auth.getToken()
-//     return token_id ? children : <Navigate to='/login'/>
-//   };
-
-// export default PrivateRoute
