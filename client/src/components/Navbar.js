eab20f6629dcc9731f2defc353aa93406e90ccdd
M A Razvi <abuzarflw@gmail.com>
2022-06-10 00:55:01 -0700

    Make navbar transparent. Change body font

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,21 +7,15 @@ import Login from './Login';
 import Auth from '../utils/auth';
 
 const AppNavbar = () => {
-  // set modal display state
-  const [showModal, setShowModal] = useState(false);
 
   return (
-      <Navbar className="flex flex-row px-6 h-fit"  variant='dark' expand='lg' style={{position:"fixed"}}>
+      <Navbar className="flex flex-row px-6 h-fit items-end justify-end"  variant='dark' expand='lg' style={{position:"fixed"}}>
 
-        <Container>
-          <div>
-            <h1 className='text-orange-700 text-xl'>Hungry Sharks</h1>
-          </div>
-        </Container>
 
         <Container className="flex flex-row items-center justify-end" fluid>
           <Navbar.Brand 
-          className="bg-[#040417] hover:bg-orange-700 text-white font-bold py-2  px-10"
+          className="hover:bg-orange-700 text-white text-2xl py-2  px-10"
+          style={{textShadow: "rgba(255, 68, 0, 0.918) 0 10px 20px"}}
           as={Link} to='/'>
             Home
           </Navbar.Brand>
@@ -30,7 +24,8 @@ const AppNavbar = () => {
           <Navbar.Collapse  id='navbar'>
             <Nav className='flex flex-row items-center  ml-auto'>
             <Nav.Link
-            className="bg-[#040417] hover:bg-orange-700 text-white font-bold py-2  px-10"
+            className="hover:bg-orange-700 text-white text-2xl py-2  px-10"
+            style={{textShadow: "rgba(255, 68, 0, 0.918) 0 10px 20px"}}
             as={Link} to='/rules'>
                 Rules
               </Nav.Link>
@@ -39,18 +34,21 @@ const AppNavbar = () => {
               {Auth.loggedIn() ? (
                 <>
                   <Nav.Link 
-                  className="bg-[#040417] hover:bg-orange-700 text-white font-bold py-2  px-10"
+                  className="hover:bg-orange-700 text-white text-2xl py-2  px-10"
+                  style={{textShadow: "rgba(255, 68, 0, 0.918) 0 10px 20px"}}
                   as={Link} to='/highscores'>
-                    See HighScores
+                    HighScores
                   </Nav.Link>
                   <br></br>
                   <Nav.Link 
-                  className="bg-[#040417] hover:bg-orange-700 text-white font-bold py-2  px-10"
+                  className="hover:bg-orange-700 text-white text-2xl py-2  px-10"
+                  style={{textShadow: "rgba(255, 68, 0, 0.918) 0 10px 20px"}}
                   onClick={Auth.logout}>Logout</Nav.Link>
                 </>
               ) : (
                 <Nav.Link 
-                className="bg-[#040417] hover:bg-orange-700 text-white font-bold py-2  px-10"
+                className="hover:bg-orange-700 text-white text-2xl py-2  px-10"
+                style={{textShadow: "rgba(255, 68, 0, 0.918) 0 10px 20px"}}
                 as={Link} to='/login'>Login</Nav.Link>
               )
               }
