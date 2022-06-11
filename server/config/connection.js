28153932b8d12079afa6bffe91261ddf467a97b1
M A Razvi <abuzarflw@gmail.com>
2022-06-11 15:49:37 -0700

    Changes to server file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/hungry-shark-cards-db', {
+const MongoDB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hungry-shark-cards-db';
+console.log(MongoDB_URI);
+mongoose.connect(MongoDB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
