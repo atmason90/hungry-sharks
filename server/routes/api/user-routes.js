c0dc7c08285e8d74aa48eee89963498bb39a9225
mateonav98 <mateonav98@gmail.com>
2022-06-10 17:33:11 -0700

    add proxy back

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -11,8 +11,8 @@ const {
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
 
-// put authMiddleware anywhere we need to send a token for verification of user.
-router.route('/').post(createUser);
+// put authMiddleware anywhere we need to send a token for verification of user
+router.route('/').post(createUser)
 
 router.route('/login').post(login);
 
