b23bc9a8b7423f1ac977e5abd5f6d2a8e00bac11
M A Razvi <abuzarflw@gmail.com>
2022-06-10 18:14:36 -0700

    Route to new rules page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Game from './components/Game';
 import HighScores from './components/HighScores';
 import Login from './components/Login'
-import Rules from './components/Rules';
 import Signup from './components/Signup';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
@@ -36,8 +35,7 @@ function App() {
             </PrivateRoute> }/>
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/rules" element={<Rules />} />
-          <Route exact path="/rools" element={<NewRules />} />
+          <Route exact path="/rules" element={<NewRules />} />
           <Route 
           exact 
           path="/highscores" 
