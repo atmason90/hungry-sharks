be7a5e78c5353c56b2b2b5534bf4bcae4474b5a1
M A Razvi <abuzarflw@gmail.com>
2022-06-12 14:15:16 -0700

    Restructure stats component

--- client/src/components/HighScores.js
+++ client/src/components/HighScores.js
@@ -1,54 +1,49 @@
-import Auth from '../utils/auth';
-import {getHighscores, getMe, getSingleHighscore} from '../utils/API'
-import React, { useState, useEffect } from 'react';
-import Stats from './Stats';
-import Navbar from "./Navbar"
-
-
+import Auth from "../utils/auth";
+import { getHighscores, getMe, getSingleHighscore } from "../utils/API";
+import React, { useState, useEffect } from "react";
+import Stats from "./Stats";
+import Navbar from "./Navbar";
 
 const HighScores = () => {
+  const [userData, setUserData] = useState({});
 
-    const [userData, setUserData] = useState({})
-    const userDataLength = Object.keys(userData).length;
+  useEffect(() => {
+    console.log("Highscore user data ", userData);
 
-    useEffect(() => {
-      const getUserHighscores = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-        if (!token) {
-          return false
-        }
-        const response = await getMe(token);
-        console.log(response)
-        if (!response.ok) {
-          throw new Error('something is wrong')
-        }
-        const user = await response.json();
-        setUserData(user);
-      }
-      catch(error) {
-      console.log(error);
-    };
-  }
-  console.log("Highscore user data ",userData);
-  
     getUserHighscores();
-    })
+  }, [userData]);
+
+  const getUserHighscores = async () => {
+    try {
+      const token = Auth.loggedIn() ? Auth.getToken() : null;
+      if (!token) {
+        return false;
+      }
+      const response = await getMe(token);
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("something is wrong");
+      }
+      const user = await response.json();
+      setUserData(user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div className='h-screen'>
-      <Navbar/>
-      <div className='pt-24'>
-          <h2 className='text-4xl text-orange-600'>
+    <div className="h-screen">
+      <Navbar />
+      <div className="pt-24">
+        <h2 className="text-4xl text-orange-600">
           {userData.stats
             ? `Viewing ${userData.username}'s Game Stats:`
-            : 'You have no stats!'}
+            : "You have no stats!"}
         </h2>
-        <Stats />
-        </div>
-      
+        <Stats  wins={userData.stats.wins} losses={userData.stats.losses}/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HighScores
+export default HighScores;
